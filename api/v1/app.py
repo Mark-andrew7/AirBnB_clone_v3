617adb6b59e50e9a43589a5d2c439a4f2a12cc9b
Mark-andrew7 <kamaumark19@gmail.com>
2024-04-29 08:48:23 +0300

    Some stats?

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,12 +12,14 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_appcontext():
     """
     Closes the storage
     """
     storage.close()
+
 
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,9 @@
 """
 Status of api
 """
-
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status", methods=["GET"])
@@ -12,3 +13,22 @@ def status():
     Returns status of api
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/api/v1/stats", methods=["GET"], strict_slashes=False)
+def get_stats():
+    """
+    Retrives number of each object
+    """
+    classes = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+    }
+    stats = {}
+    for k, v in classes.items():
+        stats[k] = storage.count(v)
+    return jsonify(stats)
